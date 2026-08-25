de6f10704fbb9343313396897aa7747a314e6285
Shilong Li <146972613+BICAamy@users.noreply.github.com>
2026-08-25 17:48:55 +0800

    feat: export projection-to-raster helpers

--- src/xvr/metrics/__init__.py
+++ src/xvr/metrics/__init__.py
@@ -1,3 +1,8 @@
 from .evaluator import Evaluator
+from .projection import project_fiducials_to_image_pixels, projection_to_image_pixels
 
-__all__ = ["Evaluator"]
+__all__ = [
+    "Evaluator",
+    "projection_to_image_pixels",
+    "project_fiducials_to_image_pixels",
+]
